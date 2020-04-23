f0c46c0514c885574aa19d9bc67d9fc9d6841384
Andre Filliettaz <andrentaz@gmail.com>
2020-04-23 12:28:53 -0300

    Add user me resource

--- src/main/kotlin/example/micronaut/controllers/UsersController.kt
+++ src/main/kotlin/example/micronaut/controllers/UsersController.kt
@@ -4,11 +4,20 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+import io.micronaut.security.token.jwt.validator.AuthenticationJWTClaimsSetAdapter
+import java.security.Principal
 
 @Controller("/api/users")
 class UsersController {
 
     @Get("/ping")
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @Produces(MediaType.TEXT_PLAIN)
     fun ping() = "pong"
+
+    @Get("/me")
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    fun userMe(principal: Principal) = (principal as AuthenticationJWTClaimsSetAdapter).attributes
 }
